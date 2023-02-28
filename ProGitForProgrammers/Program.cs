106cdafb74a07e7c7c451120e7712acf07c748a0
Juan Gregorio Tapia Ruiz <jugretaru@gmail.com>
2023-02-28 12:00:38 -0600

    comandline - from command line

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -8,6 +8,7 @@ namespace ProGitForProgramers
 		{
 			console.writeLine("Hello Wordl!");
 			console.writeLine("I just added this in visual studio");
+			console.writeLine("I just added this in comand line pro");
 		}
 	}
 }
